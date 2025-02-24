9da1e4dc340488dc43fd68172fa56d9ee29efcf4
lorenzosalti <lorenzosalti@gmail.com>
2025-02-24 18:50:35 +0100

    feat: minors discount

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@ const oversMinAge = 65;
 let ticketPrice = undefined;
 let age = prompt('Quanti anni hai?');
 let km = prompt('Quanti Km devi percorrere?');
-let ageVerify;
-let kmVerify;
 
 // data verification
-ageVerify = parseInt(age);
-kmVerify = parseInt(km);
+age = parseInt(age);
+km = parseInt(km);
 
-if ((Number.isNaN(ageVerify)) || (Number.isNaN(kmVerify))) {
+if ((Number.isNaN(age)) || (Number.isNaN(km))) {
   alert('I dati inseriti non sono corretti');
   alert('Ricaricare la pagina');
-} 
+} else {
+  ticketPrice = km * costPerKm;
+  if (age < minorsAgeLimit) {
+    ticketPrice = ticketPrice - (ticketPrice * minorsDiscount / 100);
+  };
+
+
+};
+console.log(ticketPrice);
