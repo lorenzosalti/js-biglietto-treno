09646beea3c08e4913e41bb55e177acbdf078874
lorenzosalti <lorenzosalti@gmail.com>
2025-02-24 19:09:17 +0100

    feat: price logging on console

--- js/script.js
+++ js/script.js
@@ -8,21 +8,29 @@ let ticketPrice = undefined;
 let age = prompt('Quanti anni hai?');
 let km = prompt('Quanti Km devi percorrere?');
 
-// data verification
+// defining inputs as integers
 age = parseInt(age);
 km = parseInt(km);
 
+// data verification
 if ((Number.isNaN(age)) || (Number.isNaN(km))) {
+  // errors alerts
   alert('I dati inseriti non sono corretti');
   alert('Ricaricare la pagina');
 } else {
+  // price calculation
   ticketPrice = km * costPerKm;
+  // apply discount for minors
   if (age < minorsAgeLimit) {
     ticketPrice = ticketPrice - (ticketPrice * minorsDiscount / 100);
   };
+  // apply discount for elders
   if (age >= oversMinAge) {
     ticketPrice = ticketPrice - (ticketPrice * oversDiscount / 100);
   };
+  // rounding price to 2nd decimal
   ticketPrice = ticketPrice.toFixed(2);
+  // logging the correct price
+  console.log(`Il prezzo del biglietto è: ${ticketPrice}€`);
 };
-console.log(ticketPrice);
+
