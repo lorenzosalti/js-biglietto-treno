18c7a7eed95cf1c778797b0d700c6255d4f5ef20
lorenzosalti <lorenzosalti@gmail.com>
2025-02-24 17:37:22 +0100

    feat: let variables

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const minorsDiscount = 20;
 const oversDiscount = 40;
 const minorsAgeLimit = 18;
 const oversMinAge = 65;
+let ticketPrice = undefined;
+let age = prompt('Quanti anni hai?');
+let km = prompt('Quanti Km devi percorrere?');
